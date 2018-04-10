f85b9c50a0f54d784c3c3a429ce2d69b373a0b47
Md Asif Choudhury <bdl365@yahoo.com>
2018-04-10 11:54:27 -0400

    project reorganized

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+})
